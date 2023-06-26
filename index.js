f99b645c009562e4bf9d98df41468966b185529a
barakatimothy <timothybaraka39@gmail.com>
2023-06-26 13:58:06 -0400

    complete!

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+    const url = "http://localhost:3000/users";
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, configObject)
+      .then(response => response.json())
+      .then(parsedResponse => {
+        const id = parsedResponse.id;
+        document.body.append(id);
+      })
+      .catch(error => {
+        document.body.append(error.message);
+      });
+  }
+  submitData("John Doe", "john@example.com");
